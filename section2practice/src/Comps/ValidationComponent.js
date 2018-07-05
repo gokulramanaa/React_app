1661cfc808f1e9a0e77fa18281fa32e3db5c1d3a
Gokulramanan Soundararajan <gokul_ramanan@hotmail.com>
2018-07-04 23:51:39 -0400

    secondpractice

---
+++ section2practice/src/Comps/ValidationComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+displaylene = (props) => {
+  let vari = false;
+
+  return(
+    <div>
+      <p>props.leng</p>
+    </div>
+  )
+};
+
+
+export default displaylene;
